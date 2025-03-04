febd3fe594d784274f77b78137d986cc62aee03f
Sagar Rawat <116507833+SagarRawat24@users.noreply.github.com>
2025-03-05 01:54:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {port:5173}
+// })
+
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
+  server: {
+    host: '0.0.0.0', // Allow access from outside the container
+    port: 5173,      // Keep the same port
+    strictPort: true, // Ensure it fails if port 5173 is in use
+  }
 })
