ee8b1c93dee8b65fb460cde629e12a84a3729f4c
sagar rawat <sagarrawat24291@gmail.com>
2025-03-16 17:59:55 +0530

    update vite.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,8 +11,7 @@
 
 
 
-<<<<<<< HEAD
-=======
+
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
@@ -28,7 +27,7 @@
 
 
 
->>>>>>> b646a135f68d9e1d35b3fcbf652727bb130c0a9f
+
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -37,14 +36,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-<<<<<<< HEAD
-    host: '0.0.0.0', // Allow access from outside the container
-    port: 5173,      // Keep the same port
-    strictPort: true, // Ensure it fails if port 5173 is in use
-  }
-})
-
-=======
+    // host: '0.0.0.0', // Allow access from outside the container
+    // port: 5173,      // Keep the same port
+    // strictPort: true, // Ensure it fails if port 5173 is in use
     host: '0.0.0.0',  // Allows access from anywhere
     port: 5173,       // Keep the same port
     strictPort: true, // Ensures it fails if port 5173 is in use
@@ -52,11 +46,3 @@ export default defineConfig({
     allowedHosts: 'all' // Allows all hosts
   }
 })
-
-
-
-
-
-
-
->>>>>>> b646a135f68d9e1d35b3fcbf652727bb130c0a9f
